18b2c3b0ae69c906176dc2e6dad77b9b33708e08
Adam Rao <raoadam87@gmail.com>
2021-10-06 13:47:08 +0300

    Fixes null safety issue affecting one of the packages

--- bonga/lib/views/registration_screen.dart
+++ bonga/lib/views/registration_screen.dart
@@ -2,8 +2,7 @@ import 'package:bonga/constants.dart';
 import 'package:bonga/controllers/authentication.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
-// ignore: import_of_legacy_library_into_null_safe
-import 'package:modal_progress_hud/modal_progress_hud.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 import 'components/form_field.dart';
 import 'components/major_button.dart';
